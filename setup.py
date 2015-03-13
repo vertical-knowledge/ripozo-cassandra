180c0952265ff07edad2df04c9671113bec39a7b
timmartin19 <tim.martin@vertical-knowledge.com>
2015-03-12 21:07:17 -0400

    Added .travis file

--- setup.py
+++ setup.py
@@ -20,5 +20,19 @@ setup(
     description='',
     install_requires=['ripozo', 'cqlengine'],
     tests_require=['ripozo-tests'],
-    test_suite='tests'
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: Other/Proprietary License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3'
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
